f08bd4450b602e4102da207d954946b16c8c6e03
Jaton David <david.jaton@he-arc.ch>
2024-10-31 11:15:16 +0100

    Section 8 done

--- main.js
+++ main.js
@@ -1,81 +1,94 @@
 const app = Vue.createApp({
-    data: function () {
-      return {
-        title: 'Achat de café Nespresso',
-        description : 'Site pour acheter du café Nespresso',
-        image : '/assets/images/colombia_de_cote.png',
-        link : 'https://www.he-arc.ch',
-        inStock : true,
-        stock : 1,
-        onSale : true,
-        details : ['Doux', 'Moyen', 'Puissant'],
-        carouselImages: [
-            {
-              id: 1,
-              text: 'Capsule 1',
-              image: './assets/images/colombia.png',
-            },
-            {
-              id:  2,
-              text: 'Capsule 2',
-              image: './assets/images/colombia_de_cote.png',
-            },
-            {
-              id: 3,
-              text: 'Tasse',
-              image: './assets/images/colombia_tasse.png',
-            },
-            {
-              id: 4,
-              text: 'Paquet',
-              image: './assets/images/colombia_paquet.png',
-            }
-          ],
-          productSize :[
-            {
-              id: 1,
-              text: 'S',
-              color: '#6C99C6',
-            },
-            {
-              id: 2,
-              text: 'M',
-              color: '#BF9E74',
-            },
-            {
-              id: 3,
-              text: 'L',
-              color: '#D3B1A2',
-            }
-          ],
-          cart : 0,
-
-          styles: {
-            roundButton: {
-                borderRadius: '20px',
-                padding: '10px',
-                backgroundColor: 'rgb(0, 114, 180)',
-                color: 'white',
-                cursor: 'pointer'
-              },
-            },
-      };
-      
-    },
-
-    methods: {
-        addToCart: function() {
-          this.cart += 1;
+  data: function () {
+    return {
+      action: "Achat de café",
+      brand: "Nespresso",
+      description: "Site pour acheter du café Nespresso",
+      selectedImage: 0,
+      link: "https://www.he-arc.ch",
+      inStock: true,
+      stock: 11,
+      onSale: true,
+      details: ["Doux", "Moyen", "Puissant"],
+      carouselImages: [
+        {
+          id: 1,
+          text: "Capsule 1",
+          image: "./assets/images/colombia.png",
         },
-        removeFromCart: function() {
-            if (this.cart > 0) {
-              this.cart -= 1;
-            }
-      }
+        {
+          id: 2,
+          text: "Capsule 2",
+          image: "./assets/images/colombia_de_cote.png",
+        },
+        {
+          id: 3,
+          text: "Tasse",
+          image: "./assets/images/colombia_tasse.png",
+        },
+        {
+          id: 4,
+          text: "Paquet",
+          image: "./assets/images/colombia_paquet.png",
+        },
+      ],
+      productSize: [
+        {
+          id: 1,
+          text: "S",
+          color: "#6C99C6",
+        },
+        {
+          id: 2,
+          text: "M",
+          color: "#BF9E74",
+        },
+        {
+          id: 3,
+          text: "L",
+          color: "#D3B1A2",
+        },
+      ],
+      cart: 0,
 
+      styles: {
+        roundButton: {
+          borderRadius: "20px",
+          padding: "10px",
+          backgroundColor: "rgb(0, 114, 180)",
+          color: "white",
+          cursor: "pointer",
+        },
+      },
+    };
+  },
+
+  methods: {
+    updateSelectedImage(index) {
+      this.selectedImage = index;
     },
-  });
+    addToCart: function () {
+      this.cart += 1;
+    },
+    removeFromCart: function () {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+  },
 
- 
+  computed: {
+    title() {
+      return this.action + " " + this.brand;
+    },
+    image() { return this.carouselImages[this.selectedImage].image },
 
-  const mountedApp = app.mount('#app');
+    isOutOfStock() {
+        return this.stock <= 0;
+      },
+  },
+});
+
+
+
+const mountedApp = app.mount("#app");
